0e8bd6998245441e99edcf5ba83ede5945ccc46f
CodeWithJalloh <codewithjalloh@gmail.com>
2017-07-08 18:06:35 +0100

    Constants struct file added

---
+++ Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Flickr World Wildlife Day
+//
+//  Created by wealthyjalloh on 08/07/2017.
+//  Copyright © 2017 CWJ. All rights reserved.
+//
+
+import Foundation
+
+// MARK: Constantss
+
+struct Constants {
+    
+    // 
+    
+    
+    
+}
