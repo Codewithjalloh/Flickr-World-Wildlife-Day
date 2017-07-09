81b0430e3198b3a65d4c3f409cf1e4788977d73a
CodeWithJalloh <codewithjalloh@gmail.com>
2017-07-09 18:43:33 +0100

    Configure The UI func added

--- Flickr World Wildlife Day/ViewController.swift
+++ Flickr World Wildlife Day/ViewController.swift
@@ -16,13 +16,42 @@ class ViewController: UIViewController {
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var nameTitleLabel: UILabel!
     @IBOutlet weak var getImageButton: UIButton!
-
+    
+    
+    // MARK: ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    
+    // MARK: Actions
+    @IBAction func getNewImage(_ sender: AnyObject) {
+        
         
     }
-
+    
+    
+    
+    // MARK: Configure The UI
+    private func setUIEnable(trueOrFalse enabled: Bool) {
+        nameTitleLabel.isEnabled = enabled
+        getImageButton.isEnabled = enabled
+        
+        if enabled {
+            // get button alpha 100% visible
+            getImageButton.alpha = 1.0
+        } else {
+            // get button alpha 50% visible
+            getImageButton.alpha = 0.5
+        }
+    }
+    
+    // MARK: Make Network Request
+    private func getImageFromFlickr() {
+        
+    }
+    
     
 
 }
