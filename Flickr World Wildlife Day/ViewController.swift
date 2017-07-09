714dc46cce60a820d11d3b6a6c30cbcb867e0aae
CodeWithJalloh <codewithjalloh@gmail.com>
2017-07-09 18:54:33 +0100

    getImageFromFlickr updated

--- Flickr World Wildlife Day/ViewController.swift
+++ Flickr World Wildlife Day/ViewController.swift
@@ -4,12 +4,14 @@
 //
 //  Created by codewithjalloh on 08/07/2017.
 //  Copyright © 2017 CWJ. All rights reserved.
-//
+
+// full get galleries link
+//  https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=YOURKEY&gallery_id=72157677539266623&format=json&nojsoncallback=1&auth_token=72157683329763343-b1b1229db93edbf4&api_sig=05cf135ee202cc12694354f493653bce
+
 
 import UIKit
 
 class ViewController: UIViewController {
-    
     
     // MARK: Outlets
     
@@ -24,14 +26,13 @@ class ViewController: UIViewController {
         
     }
     
-    
     // MARK: Actions
     @IBAction func getNewImage(_ sender: AnyObject) {
-        
+        // call the setUIEnable and getImageFromFlickr func
+        setUIEnable(trueOrFalse: false)
+        getImageFromFlickr()
         
     }
-    
-    
     
     // MARK: Configure The UI
     private func setUIEnable(trueOrFalse enabled: Bool) {
@@ -50,8 +51,24 @@ class ViewController: UIViewController {
     // MARK: Make Network Request
     private func getImageFromFlickr() {
         
+        let methodParameters = [
+            
+            // Call from the Constants.swift file
+            
+            Constants.FlickrParameterKeys.Method : Constants.FlickrParameterValues.GalleryPhotosMethod,
+            Constants.FlickrParameterKeys.APIKey : Constants.FlickrParameterValues.APIKey,
+            Constants.FlickrParameterKeys.GalleryID : Constants.FlickrParameterValues.GalleryID,
+            Constants.FlickrParameterKeys.Extras : Constants.FlickrParameterValues.MediumURL,
+            Constants.FlickrParameterKeys.Format : Constants.FlickrParameterValues.ResonseFormat,
+            Constants.FlickrParameterKeys.NoJSONCallback : Constants.FlickrParameterValues.DisableJSONCallbacl
+            
+        ]
+        
     }
     
+    
+    
+    private
     
 
 }
