4747f42635ecedd5d8eafcbc6cea5850500ebf97
CodeWithJalloh <codewithjalloh@gmail.com>
2017-07-09 18:35:27 +0100

    Outlets added

--- Flickr World Wildlife Day/ViewController.swift
+++ Flickr World Wildlife Day/ViewController.swift
@@ -9,17 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var nameTitleLabel: UILabel!
+    @IBOutlet weak var getImageButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 
